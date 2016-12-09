87115e0f3e9e05f538eebb60597f4b89fe7148c6
Alex Knipfer <alexanderknipfer@gmail.com>
2016-12-09 17:30:37 -0600

    intialize empty map of unoccupied tiles

--- src/com/chess/engine/board/Tile.java
+++ src/com/chess/engine/board/Tile.java
@@ -5,20 +5,45 @@ package com.chess.engine.board;
  */
 
 import com.chess.engine.pieces.Piece;
+import com.google.common.collect.ImmutableMap;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public abstract class Tile {
 
         //only allow tile coordinate to be set in constructor
     protected final int tileCoordinate;
 
+    private static final Map<Integer, EmptyTile> EMPTY_TILES = createAllPossibleEmptyTiles();
+
         //give the tile an initial coordinate
-    Tile(int tileCoordinate) {
+    private Tile(int tileCoordinate) {
         this.tileCoordinate = tileCoordinate;
     }
 
     public abstract boolean isTileOccupied();
 
     public abstract Piece getPiece();
+
+    public static Tile createTile(final int tileCoordinate, final Piece piece) {
+        return piece != null ? new OccupiedTile(tileCoordinate, piece) : EMPTY_TILES.get(tileCoordinate);
+    }
+
+        //initialize map of empty tiles
+    private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles() {
+
+        final Map<Integer, EmptyTile> emptyTileMap = new HashMap<>();
+
+            //place empty tiles
+        for(int i = 0; i < 64; i++) {
+            emptyTileMap.put(i, new EmptyTile(i));
+        }
+
+            //return a immutable copy so tiles can't be changed
+        return ImmutableMap.copyOf(emptyTileMap);
+
+    }
 
         //represents a tile without a piece
     public static final class EmptyTile extends Tile {
