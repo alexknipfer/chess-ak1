2c06d85c4f54ffa1f1fb7decdeaaf881b1ebcdfa
Alex Knipfer <alexanderknipfer@gmail.com>
2016-12-10 18:07:44 -0600

    added Move, Board, and Alliance classes

---
+++ src/com/chess/engine/Alliance.java
@@ -0,0 +1,7 @@
+package com.chess.engine;
+
+/**
+ * Created by alex on 12/10/16.
+ */
+public enum Alliance {
+}
---
+++ src/com/chess/engine/board/Board.java
@@ -0,0 +1,7 @@
+package com.chess.engine.board;
+
+/**
+ * Created by alex on 12/10/16.
+ */
+public class Board {
+}
