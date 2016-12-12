7b628a066d794036e72b7f38841301ed321940bb
Alex Knipfer <alexanderknipfer@gmail.com>
2016-12-11 20:14:41 -0600

    check to see if legal tile

--- src/com/chess/engine/board/BoardUtils.java
+++ src/com/chess/engine/board/BoardUtils.java
@@ -4,4 +4,13 @@ package com.chess.engine.board;
  * Created by alex on 12/11/16.
  */
 public class BoardUtils {
+
+    private BoardUtils() {
+        throw new RuntimeException("You cannot instantiate me!");
+    }
+
+        //check to see if move is valid (i.e is within board bounds)
+    public static boolean isValidTileCoordinate(int coordinate) {
+        return coordinate >= 0 && coordinate < 64;
+    }
 }
