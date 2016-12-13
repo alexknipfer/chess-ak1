a6b8fda5483ec11c94f32f06169d49835897ee0e
Alex Knipfer <alexanderknipfer@gmail.com>
2016-12-13 00:08:27 -0600

    constructors for multiple move types

--- src/com/chess/engine/board/Move.java
+++ src/com/chess/engine/board/Move.java
@@ -1,7 +1,41 @@
 package com.chess.engine.board;
 
+import com.chess.engine.pieces.Piece;
+
 /**
  * Created by alex on 12/10/16.
  */
-public class Move {
+public abstract class Move {
+
+    final Board board;
+    final Piece movedPiece;
+    final int destinationCoordinate;
+
+    private Move(final Board board, final Piece movedPiece, final int destinationCoordinate) {
+        this.board = board;
+        this.movedPiece = movedPiece;
+        this.destinationCoordinate = destinationCoordinate;
+    }
+
+        //moving a piece to an unoccupied tile
+    public static final class MajorMove extends Move {
+
+        public MajorMove(final Board board, final Piece movedPiece, final int destinationCoordinate) {
+            super(board, movedPiece, destinationCoordinate);
+        }
+
+    }
+
+        //moving a piece to a tile occupied by opposite color
+    public static final class AttackMove extends Move {
+
+        final Piece attackedPiece;
+
+        public AttackMove(final Board board, final Piece movedPiece, final int destinationCoordinate, final Piece attackedPiece) {
+            super(board, movedPiece, destinationCoordinate);
+            this.attackedPiece = attackedPiece;
+        }
+
+    }
+
 }
