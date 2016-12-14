ed88389d387a276169aa81327fa61e55ea7f6c1f
Alex Knipfer <alexanderknipfer@gmail.com>
2016-12-14 14:27:33 -0600

    added Bishop class, calculate and check valid move coordinates

---
+++ src/com/chess/engine/pieces/Bishop.java
@@ -0,0 +1,85 @@
+package com.chess.engine.pieces;
+
+import com.chess.engine.Alliance;
+import com.chess.engine.board.Board;
+import com.chess.engine.board.BoardUtils;
+import com.chess.engine.board.Move;
+import com.chess.engine.board.Tile;
+import com.google.common.collect.ImmutableList;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Created by alex on 12/14/16.
+ */
+public class Bishop extends Piece {
+
+    private final static int[] CANDIDATE_MOVE_VECTOR_COORDINATES = { -9, -7, 7, 9};
+
+
+    Bishop(int piecePosition, Alliance pieceAlliance) {
+        super(piecePosition, pieceAlliance);
+    }
+
+    @Override
+    public Collection<Move> calculateLegalMoves(Board board) {
+
+        final List<Move> legalMoves = new ArrayList<>();
+
+            //loop through vectors of possible valid moves of a bishop
+        for(final int candidateCoordinateOffset : CANDIDATE_MOVE_VECTOR_COORDINATES) {
+
+            int candidateDestinationCoordinate = this.piecePosition;
+
+            while(BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)) {
+
+                    //check to see if bishop is in the first or eighth column
+                if(isFirstColumnExclusion(candidateDestinationCoordinate, candidateCoordinateOffset) ||
+                        isEightColumnExclusion(candidateDestinationCoordinate, candidateCoordinateOffset)) {
+                    break;
+                }
+
+                    //apply offset
+                candidateDestinationCoordinate += candidateCoordinateOffset;
+
+                    //check if the tile "moving too" is a valid coordinate
+                if(BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)) {
+
+                    final Tile candidateDestinationTile = board.getTile(candidateDestinationCoordinate);
+
+                        //check to see if destination tile does not have a piece on it
+                    if(!candidateDestinationTile.isTileOccupied()) {
+                        legalMoves.add(new Move.MajorMove(board, this, candidateDestinationCoordinate));
+                    }
+
+                        //destination tile HAS a piece on it (occupied)
+                    else {
+                        final Piece pieceAtDestination = candidateDestinationTile.getPiece();
+                        final Alliance pieceAlliance = pieceAtDestination.getPieceAlliance();
+
+                        //check to see if occupied piece is of opposite color
+                        if(this.pieceAlliance != pieceAlliance) {
+                            legalMoves.add(new Move.AttackMove(board, this, candidateDestinationCoordinate, pieceAtDestination));
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+
+        return ImmutableList.copyOf(legalMoves);
+    }
+
+        //check to see if bishop is currently in the first column
+    private static boolean isFirstColumnExclusion(final int currentPosition, final int candidateOffset) {
+        return BoardUtils.FIRST_COLUMN[currentPosition] && (candidateOffset == -9 || candidateOffset == 7);
+    }
+
+        //check to see if the bishop is currently in the eighth column
+    private static boolean isEightColumnExclusion(final int currentPosition, final int candidateOffset) {
+        return BoardUtils.EIGHTH_COLUMN[currentPosition] && (candidateOffset == -7 || candidateOffset == 9);
+    }
+
+}
